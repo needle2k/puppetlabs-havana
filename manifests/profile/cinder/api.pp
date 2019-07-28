c15cee41bf6bf9c0695b71cef480e3c8937a4651
needle2k <manga@comic.com>
2019-07-28 19:22:33 +0200

    Update api.pp

--- manifests/profile/cinder/api.pp
+++ manifests/profile/cinder/api.pp
@@ -1,23 +1,23 @@
 # The profile for installing the Cinder API
-class havana::profile::cinder::api {
+class stein::profile::cinder::api {
 
-  havana::resources::controller { 'cinder': }
-  havana::resources::database { 'cinder': }
-  havana::resources::firewall { 'Cinder API': port => '8776', }
+  stein::resources::controller { 'cinder': }
+  stein::resources::database { 'cinder': }
+  stein::resources::firewall { 'Cinder API': port => '8776', }
 
   class { '::cinder::keystone::auth':
-    password         => hiera('havana::cinder::password'),
-    public_address   => hiera('havana::controller::address::api'),
-    admin_address    => hiera('havana::controller::address::management'),
-    internal_address => hiera('havana::controller::address::management'),
-    region           => hiera('havana::region'),
+    password         => hiera('stein::cinder::password'),
+    public_address   => hiera('stein::controller::address::api'),
+    admin_address    => hiera('stein::controller::address::management'),
+    internal_address => hiera('stein::controller::address::management'),
+    region           => hiera('stein::region'),
   }
 
-  include ::havana::common::cinder
+  include ::stein::common::cinder
 
   class { '::cinder::api':
-    keystone_password  => hiera('havana::cinder::password'),
-    keystone_auth_host => hiera('havana::controller::address::management'),
+    keystone_password  => hiera('stein::cinder::password'),
+    keystone_auth_host => hiera('stein::controller::address::management'),
     enabled            => true,
   }
 
