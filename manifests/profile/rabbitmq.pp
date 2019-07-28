08d5fc321cee0f570c1549b5bcb0465e16c9809b
needle2k <manga@comic.com>
2019-07-28 19:20:15 +0200

    Update rabbitmq.pp

--- manifests/profile/rabbitmq.pp
+++ manifests/profile/rabbitmq.pp
@@ -1,8 +1,8 @@
 # The profile to install rabbitmq and set the firewall
-class havana::profile::rabbitmq {
+class stein::profile::rabbitmq {
   class { '::nova::rabbitmq':
-    userid             => hiera('havana::rabbitmq::user'),
-    password           => hiera('havana::rabbitmq::password'),
-    cluster_disk_nodes => hiera('havana::controller::address::management'),
+    userid             => hiera('stein::rabbitmq::user'),
+    password           => hiera('stein::rabbitmq::password'),
+    cluster_disk_nodes => hiera('stein::controller::address::management'),
   }
 }
