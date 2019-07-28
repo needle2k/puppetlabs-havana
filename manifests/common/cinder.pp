73a598e6f11c315e809ef0f283c09106b779d3d7
needle2k <manga@comic.com>
2019-07-28 19:11:03 +0200

    Update cinder.pp

--- manifests/common/cinder.pp
+++ manifests/common/cinder.pp
@@ -1,12 +1,12 @@
 # Common class for cinder installation
 # Private, and should not be used on its own
-class havana::common::cinder {
+class stein::common::cinder {
   class { '::cinder':
-    sql_connection    => $::havana::resources::connectors::cinder,
-    rabbit_host       => hiera('havana::controller::address::management'),
-    rabbit_userid     => hiera('havana::rabbitmq::user'),
-    rabbit_password   => hiera('havana::rabbitmq::password'),
-    debug             => hiera('havana::debug'),
-    verbose           => hiera('havana::verbose'),
+    sql_connection    => $::stein::resources::connectors::cinder,
+    rabbit_host       => hiera('stein::controller::address::management'),
+    rabbit_userid     => hiera('stein::rabbitmq::user'),
+    rabbit_password   => hiera('stein::rabbitmq::password'),
+    debug             => hiera('stein::debug'),
+    verbose           => hiera('stein::verbose'),
   }
 }
