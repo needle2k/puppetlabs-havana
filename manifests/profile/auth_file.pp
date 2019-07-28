76f35e430168948adda75c196bb34246c14e6e5a
needle2k <manga@comic.com>
2019-07-28 19:14:20 +0200

    Update auth_file.pp

--- manifests/profile/auth_file.pp
+++ manifests/profile/auth_file.pp
@@ -1,9 +1,9 @@
 # The profile to install an OpenStack specific mysql server
-class havana::profile::auth_file {
+class stein::profile::auth_file {
   class { '::openstack::auth_file':
     admin_tenant    => 'admin',
-    admin_password  => hiera('havana::keystone::admin_password'),
-    region_name     => hiera('havana::region'),
-    controller_node => hiera('havana::controller::address::api'),
+    admin_password  => hiera('stein::keystone::admin_password'),
+    region_name     => hiera('stein::region'),
+    controller_node => hiera('stein::controller::address::api'),
   }
 }
