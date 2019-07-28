01d362cb4f06da2cbd457086b63d51b85cc20bcb
needle2k <manga@comic.com>
2019-07-28 19:27:34 +0200

    Update api.pp

--- manifests/profile/heat/api.pp
+++ manifests/profile/heat/api.pp
@@ -1,48 +1,48 @@
 # The profile for installing the heat API
-class havana::profile::heat::api {
-  havana::resources::controller { 'heat': }
-  havana::resources::database { 'heat': }
-  havana::resources::firewall { 'Heat API': port     => '8004', }
-  havana::resources::firewall { 'Heat CFN API': port => '8000', }
+class stein::profile::heat::api {
+  stein::resources::controller { 'heat': }
+  stein::resources::database { 'heat': }
+  stein::resources::firewall { 'Heat API': port     => '8004', }
+  stein::resources::firewall { 'Heat CFN API': port => '8000', }
 
-  $controller_management_address = hiera('havana::controller::address::management')
+  $controller_management_address = hiera('stein::controller::address::management')
 
   class { '::heat::keystone::auth':
-    password         => hiera('havana::heat::password'),
-    public_address   => hiera('havana::controller::address::api'),
-    admin_address    => hiera('havana::controller::address::management'),
-    internal_address => hiera('havana::controller::address::management'),
-    region           => hiera('havana::region'),
+    password         => hiera('stein::heat::password'),
+    public_address   => hiera('stein::controller::address::api'),
+    admin_address    => hiera('stein::controller::address::management'),
+    internal_address => hiera('stein::controller::address::management'),
+    region           => hiera('stein::region'),
   }
 
   class { '::heat::keystone::auth_cfn': 
-    password         => hiera('havana::heat::password'),
-    public_address   => hiera('havana::controller::address::api'),
-    admin_address    => hiera('havana::controller::address::management'),
-    internal_address => hiera('havana::controller::address::management'),
-    region           => hiera('havana::region'),
+    password         => hiera('stein::heat::password'),
+    public_address   => hiera('stein::controller::address::api'),
+    admin_address    => hiera('stein::controller::address::management'),
+    internal_address => hiera('stein::controller::address::management'),
+    region           => hiera('stein::region'),
   }
 
   class { '::heat':
-    sql_connection    => $::havana::resources::connectors::heat,
-    rabbit_host       => hiera('havana::controller::address::management'),
-    rabbit_userid     => hiera('havana::rabbitmq::user'),
-    rabbit_password   => hiera('havana::rabbitmq::password'),
-    debug             => hiera('havana::debug'),
-    verbose           => hiera('havana::verbose'),
-    keystone_host     => hiera('havana::controller::address::management'),
-    keystone_password => hiera('havana::heat::password'),
+    sql_connection    => $::stein::resources::connectors::heat,
+    rabbit_host       => hiera('stein::controller::address::management'),
+    rabbit_userid     => hiera('stein::rabbitmq::user'),
+    rabbit_password   => hiera('stein::rabbitmq::password'),
+    debug             => hiera('stein::debug'),
+    verbose           => hiera('stein::verbose'),
+    keystone_host     => hiera('stein::controller::address::management'),
+    keystone_password => hiera('stein::heat::password'),
   }
 
   class { '::heat::api':
-    bind_host => hiera('havana::controller::address::api'),
+    bind_host => hiera('stein::controller::address::api'),
   }
 
   class { '::heat::api_cfn':
-    bind_host => hiera('havana::controller::address::api'),
+    bind_host => hiera('stein::controller::address::api'),
   }
 
   class { '::heat::engine':
-    auth_encryption_key => hiera('havana::heat::encryption_key'),
+    auth_encryption_key => hiera('stein::heat::encryption_key'),
   }
 }
