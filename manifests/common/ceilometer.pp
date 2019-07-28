bbb1d4c3c2cac25bcffc164b1071711fadf33815
needle2k <manga@comic.com>
2019-07-28 19:10:17 +0200

    Update ceilometer.pp

--- manifests/common/ceilometer.pp
+++ manifests/common/ceilometer.pp
@@ -1,27 +1,27 @@
 # Common class for ceilometer installation
 # Private, and should not be used on its own
-class havana::common::ceilometer {
-  $is_controller = $::havana::profile::base::is_controller
+class stein::common::ceilometer {
+  $is_controller = $::stein::profile::base::is_controller
 
-  $controller_management_address = hiera('havana::controller::address::management')
+  $controller_management_address = hiera('stein::controller::address::management')
 
-  $mongo_password = hiera('havana::ceilometer::mongo::password')
+  $mongo_password = hiera('stein::ceilometer::mongo::password')
   $mongo_connection = 
     "mongodb://${controller_management_address}:27017/ceilometer"
 
   class { '::ceilometer':
-    metering_secret => hiera('havana::ceilometer::meteringsecret'),
-    debug           => hiera('havana::debug'),
-    verbose         => hiera('havana::verbose'),
+    metering_secret => hiera('stein::ceilometer::meteringsecret'),
+    debug           => hiera('stein::debug'),
+    verbose         => hiera('stein::verbose'),
     rabbit_hosts    => [$controller_management_address],
-    rabbit_userid   => hiera('havana::rabbitmq::user'),
-    rabbit_password => hiera('havana::rabbitmq::password'),
+    rabbit_userid   => hiera('stein::rabbitmq::user'),
+    rabbit_password => hiera('stein::rabbitmq::password'),
   }
 
   class { '::ceilometer::api':
     enabled           => $is_controller,
     keystone_host     => $controller_management_address,
-    keystone_password => hiera('havana::ceilometer::password'),
+    keystone_password => hiera('stein::ceilometer::password'),
   }
 
   class { '::ceilometer::db':
@@ -30,8 +30,8 @@ class havana::common::ceilometer {
 
   class { '::ceilometer::agent::auth':
     auth_url      => "http://${controller_management_address}:5000/v2.0",
-    auth_password => hiera('havana::ceilometer::password'),
-    auth_region   => hiera('havana::region'),
+    auth_password => hiera('stein::ceilometer::password'),
+    auth_region   => hiera('stein::region'),
   }
 }
 
