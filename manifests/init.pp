b171b55c803368251752f936b06800cf6c81d0b1
needle2k <manga@comic.com>
2019-07-28 18:53:07 +0200

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,14 @@
-# == Class: havana
+# == Class: stein
 #
 # === Authors
 #
 # Christian Hoge <chris.hoge@puppetlabs.com>
-#
 # === Copyright
 #
 # Copyright 2013 Puppet Labs.
 #
-class havana {
+# Forked by 
+# needle2k <manga@comic.com>
+#
+class stein {
 }
