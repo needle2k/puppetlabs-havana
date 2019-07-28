bb30e620dc51ebbad96d530d6358d9318d45c319
needle2k <manga@comic.com>
2019-07-28 19:23:05 +0200

    Update volume.pp

--- manifests/profile/cinder/volume.pp
+++ manifests/profile/cinder/volume.pp
@@ -1,13 +1,13 @@
 # The profile to install the volume service
-class havana::profile::cinder::volume {
+class stein::profile::cinder::volume {
 
-  havana::resources::firewall { 'ISCSI API': port => '3260', }
+  stein::resources::firewall { 'ISCSI API': port => '3260', }
 
-  include ::havana::common::cinder
+  include ::stein::common::cinder
 
   class { '::cinder::setup_test_volume': 
     volume_name => 'cinder-volumes',
-    size        => hiera('havana::cinder::volume_size')
+    size        => hiera('stein::cinder::volume_size')
   } ->
 
   class { '::cinder::volume':
@@ -16,7 +16,7 @@ class havana::profile::cinder::volume {
   }
 
   class { '::cinder::volume::iscsi':
-    iscsi_ip_address  => hiera('havana::storage::address::management'),
+    iscsi_ip_address  => hiera('stein::storage::address::management'),
     volume_group      => 'cinder-volumes',
   }
 }
