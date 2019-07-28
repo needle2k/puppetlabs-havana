0074e6eb4edf4bb4896c3912d57230d978065c44
needle2k <manga@comic.com>
2019-07-28 19:29:07 +0200

    Update server.pp

--- manifests/profile/neutron/server.pp
+++ manifests/profile/neutron/server.pp
@@ -1,22 +1,22 @@
 # The profile to set up the neutron server
-class havana::profile::neutron::server {
-  havana::resources::controller { 'neutron': }
-  havana::resources::database { 'neutron': }
-  havana::resources::firewall { 'Neutron API': port => '9696', }
+class stein::profile::neutron::server {
+  stein::resources::controller { 'neutron': }
+  stein::resources::database { 'neutron': }
+  stein::resources::firewall { 'Neutron API': port => '9696', }
 
   class { '::neutron::keystone::auth':
-    password         => hiera('havana::neutron::password'),
-    public_address   => hiera('havana::controller::address::api'),
-    admin_address    => hiera('havana::controller::address::management'),
-    internal_address => hiera('havana::controller::address::management'),
-    region           => hiera('havana::region'),
+    password         => hiera('stein::neutron::password'),
+    public_address   => hiera('stein::controller::address::api'),
+    admin_address    => hiera('stein::controller::address::management'),
+    internal_address => hiera('stein::controller::address::management'),
+    region           => hiera('stein::region'),
   }
 
   class { '::neutron::server':
-    auth_host     => hiera('havana::controller::address::management'),
-    auth_password => hiera('havana::neutron::password'),
+    auth_host     => hiera('stein::controller::address::management'),
+    auth_password => hiera('stein::neutron::password'),
     enabled       => true,
   }
 
-  include ::havana::common::neutron
+  include ::stein::common::neutron
 }
