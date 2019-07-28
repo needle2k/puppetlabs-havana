d31364d85ddd0f9941023b29c83f0b33e31d5ab9
needle2k <manga@comic.com>
2019-07-28 19:26:54 +0200

    Update registry.pp

--- manifests/profile/glance/registry.pp
+++ manifests/profile/glance/registry.pp
@@ -1,4 +1,4 @@
 # Deprecated. The profile to set up the Glance registry
-class havana::profile::glance::registry {
+class stein::profile::glance::registry {
   notify { 'TODO: glance::registry profile': }
 }
