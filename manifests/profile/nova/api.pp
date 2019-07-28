fec93131d1a479bc06d45d280722fdfa2edee024
needle2k <manga@comic.com>
2019-07-28 19:29:39 +0200

    Update api.pp

--- manifests/profile/nova/api.pp
+++ manifests/profile/nova/api.pp
@@ -1,20 +1,19 @@
 # The profile to set up the Nova controller (several services)
-class havana::profile::nova::api {
-  havana::resources::controller { 'nova': }
-  havana::resources::database { 'nova': }
-  havana::resources::firewall { 'Nova API': port => '8774', }
-  havana::resources::firewall { 'Nova Metadata': port => '8775', }
-  havana::resources::firewall { 'Nova NoVncProxy': port => '6080', }
+class stein::profile::nova::api {
+  stein::resources::controller { 'nova': }
+  stein::resources::database { 'nova': }
+  stein::resources::firewall { 'Nova API': port => '8774', }
+  stein::resources::firewall { 'Nova Metadata': port => '8775', }
+  stein::resources::firewall { 'Nova NoVncProxy': port => '6080', }
 
   class { '::nova::keystone::auth':
-    password         => hiera('havana::nova::password'),
-    public_address   => hiera('havana::controller::address::api'),
-    admin_address    => hiera('havana::controller::address::management'),
-    internal_address => hiera('havana::controller::address::management'),
-    region           => hiera('havana::region'),
+    password         => hiera('stein::nova::password'),
+    public_address   => hiera('stein::controller::address::api'),
+    admin_address    => hiera('stein::controller::address::management'),
+    internal_address => hiera('stein::controller::address::management'),
+    region           => hiera('stein::region'),
     cinder           => true,
   }
 
-  include ::havana::common::nova
+  include ::stein::common::nova
 }
-
